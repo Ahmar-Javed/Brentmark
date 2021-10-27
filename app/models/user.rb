5013074fad44846b8df2cda62c0708214d4ce6c1
AhmerJaved <ahmer.javed@square63.org>
2021-10-27 10:40:53 +0500

    Adding Regex for password field

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+   validates :password, format: {with: /(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}/,message: " -Password must contain at least (1) special characters. Password must contain at least (1) uppercase letter. Password must contain at least (1) lowercase letter. Password must be at least 8 characters long."}
+
 end
