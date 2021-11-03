40093b46d5a7572df4247245fd5ebe5dbabe8161
Ahmer-Javed <ahmer.javed@square63.org>
2021-11-03 12:18:48 +0500

    Add a csv downloader to excel sheet

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,10 +1,14 @@
 class Admin::UsersController< ApplicationController
- 
+  require 'csv'
   def index 
     if params[:query].present?
       @users = User.search_username(params[:query])
     else
       @users = User.paginate(page: params[:page], per_page: 5)
+      respond_to do |format|
+        format.html
+        format.csv {send_data @users.to_csv}
+      end
     end
   end
   
--- app/models/user.rb
+++ app/models/user.rb
@@ -35,5 +35,13 @@ class User < ApplicationRecord
       end
    end
   end
-
+  
+  def self.to_csv
+    CSV.generate do |csv|
+      csv << column_names
+      all.each do |user|
+        csv<< user.attributes.values_at(*column_names) 
+      end
+    end
+  end  
 end
